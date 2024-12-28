f597c4ffc3903a430fa116092f1090f6f3c015de
Alvi Fricke <alviafricke@gmail.com>
2024-12-28 12:11:07 -0300

    mailing

--- src/scripts/mailer.js
+++ src/scripts/mailer.js
@@ -1,17 +1,17 @@
-// Importa EmailJS desde el paquete instalado
 import emailjs from '@emailjs/browser';
 
 // Inicializa EmailJS con tu clave pública
 emailjs.init("kSZcewJQOopIOWIvD");
 
-// Obtiene el formulario por su ID
 const form = document.getElementById("contactForm");
 
 if (form) {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log(form);
-    
+
+const formData = new FormData(this);
+    console.log(Object.fromEntries(formData.entries()));
+
     emailjs
       .sendForm("service_k4blygv", "template_ng1cy4u", form)
       .then(
